32744895b131980cb6ce3a2e593f4f07c12f0a48
Patrick Kennedy <87878545+patrick4k@users.noreply.github.com>
2024-09-12 15:31:45 -0400

    Removed reboot and added finish dialog.

--- setup/setup.bash
+++ setup/setup.bash
@@ -45,4 +45,4 @@ sudo dpkg -i libmavsdk-dev_1.4.16_ubuntu20.04_amd64.deb
 perl find_ftdi_id.pl
 
 sudo usermod -a -G dialout $USER
-sudo reboot
+echo "Setup exiting, reboot device to apply changes..."
