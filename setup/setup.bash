653a1cbdb2ad5ea39e9c36f53473ea4050c34ecd
Patrick Kennedy <87878545+patrick4k@users.noreply.github.com>
2024-09-12 17:12:34 -0400

    Update setup.bash

--- setup/setup.bash
+++ setup/setup.bash
@@ -15,7 +15,7 @@ sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 sudo apt install ros-humble-desktop -y
 sudo apt install ros-dev-tools -y
 touch ~/.bash_aliases # aliases for ros and more
