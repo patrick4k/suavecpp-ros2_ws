7b6be89ca3512ace99e7d3156f009795da44f3c8
Patrick Kennedy <87878545+patrick4k@users.noreply.github.com>
2024-09-12 16:25:58 -0400

    Added "-y" to all apt install.

--- setup/setup.bash
+++ setup/setup.bash
@@ -1,43 +1,43 @@
 # init setup
-sudo apt install openssh-server # ssh
-sudo apt install solaar # drivers for the keyboard+touchpad
+sudo apt install openssh-server -y# ssh
+sudo apt install solaar -y# drivers for the keyboard+touchpad
 
 # install ros2 (https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debs.html)
 locale  # check for UTF-8
-sudo apt update && sudo apt install locales
+sudo apt update && sudo apt install locales -y
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 locale  # verify settings
-sudo apt install software-properties-common
+sudo apt install software-properties-common -y
 sudo add-apt-repository universe
 sudo apt update && sudo apt install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update
 sudo apt upgrade
-sudo apt install ros-humble-desktop
-sudo apt install ros-dev-tools
+sudo apt install ros-humble-desktop -y
+sudo apt install ros-dev-tools -y
 touch ~/.bash_aliases # aliases for ros and more
 echo "alias ros='source /opt/ros/humble/setup.bash'" >> ~/.bash_aliases
 
 # install librealsense
 sudo mkdir -p /etc/apt/keyrings
 curl -sSf https://librealsense.intel.com/Debian/librealsense.pgp | sudo tee /etc/apt/keyrings/librealsense.pgp > /dev/null
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https -y
 echo "deb [signed-by=/etc/apt/keyrings/librealsense.pgp] https://librealsense.intel.com/Debian/apt-repo `lsb_release -cs` main" | \
 sudo tee /etc/apt/sources.list.d/librealsense.list
 sudo apt-get update
-sudo apt-get install librealsense2-dkms
-sudo apt-get install librealsense2-utils
-sudo apt-get install librealsense2-dev
-sudo apt-get install librealsense2-dbg
+sudo apt-get install librealsense2-dkms -y
+sudo apt-get install librealsense2-utils -y
+sudo apt-get install librealsense2-dev -y
+sudo apt-get install librealsense2-dbg -y
 
 # rtabmaps_ros
-sudo apt install ros-$ROS_DISTRO-rtabmap-ros
+sudo apt install ros-$ROS_DISTRO-rtabmap-ros -y
 
 # other tools
-sudo apt install libpcl-dev
+sudo apt install libpcl-dev -y
 sudo dpkg -i libmavsdk-dev_1.4.16_ubuntu20.04_amd64.deb
 
 # add persistant environment variable for mavlink serial connection
