e195c4f3e3720eca8a4506a4cdcf9ec4aec02096
patrick4k <pmkennedy4@gmail.com>
2024-09-13 12:17:07 -0400

    Added startup task to masking controller.

--- src/suave_launch/src/controllers/SuaveMaskingController.cpp
+++ src/suave_launch/src/controllers/SuaveMaskingController.cpp
@@ -4,29 +4,99 @@
 
 #include "SuaveMaskingController.h"
 
-#include "../masking_pid/MaskingSubscriber.h"
+#include "../common/SystemTask.h"
 #include "../ros/RosNodeSpinner.h"
+#include "../vio/CloudExporter.h"
+#include "../vio/VIOBridge.h"
+#include "../masking_pid/MaskingSubscriber.h"
 
 #define sleep(sec) std::this_thread::sleep_for(std::chrono::microseconds(static_cast<int>(sec * 1e6)));
 
+#define try_mav(act, success) \
+{\
+    suave_log << "Starting " << #act << std::endl; \
+    if (m_end_controller)     \
+    {\
+        suave_err << "m_end_controller = true" << std::endl; \
+        this->shutdown();\
+        return;\
+    }\
+    const auto act_result = act; \
+    if (act_result != success) \
+    { \
+        suave_err << #act << " failed: " << act_result << std::endl; \
+        this->shutdown(); \
+        return; \
+    } \
+    suave_log << #act << ": " << act_result << std::endl;\
+}
+
+#define try_action(act) try_mav(act, Action::Result::Success)
+#define try_offboard(act) try_mav(act, Offboard::Result::Success)
+#define try_tune(act) try_mav(act, Tune::Result::Success)
+
 void SuaveMaskingController::start() {
-    RosNodeSpinner spinner{};
+    // Create realsense and rtabmap nodes
+
+    auto realsense_task = std::make_shared<SystemTask>(
+        std::vector<std::string>{
+            "source /opt/ros/humble/setup.bash",
+            "ros2 launch realsense2_camera rs_launch.py enable_gyro:=true enable_accel:=true unite_imu_method:=1 enable_infra1:=true enable_infra2:=true enable_sync:=true"
+        }
+    );
+    m_task.push_back(realsense_task);
+
+    auto rtabmap_task = std::make_shared<SystemTask>(
+        std::vector<std::string>{
+            "source /opt/ros/humble/setup.bash",
+            "ros2 param set /camera/camera depth_module.emitter_enabled 0",
+            "ros2 launch rtabmap_examples realsense_d435i_infra.launch.py"
+        }
+    );
+    m_task.push_back(rtabmap_task);
+
+    // Create ROS spinner and add vio bridge and cloud exporter node
+    auto vio_spinner = std::make_shared<RosNodeSpinner>();
+    m_task.push_back(vio_spinner);
+
+    const auto vio_bridge_node = std::make_shared<VIOBridge>(m_drone->system(), m_drone->initial_heading_rad());
+    vio_spinner->add_node(vio_bridge_node);
+
+    const auto cloud_exporter_node = std::make_shared<CloudExporter>();
+    vio_spinner->add_node(cloud_exporter_node);
+
+    auto masking_spinner = std::make_shared<RosNodeSpinner>();
+    m_task.push_back(masking_spinner);
+
     auto masking_subscriber = std::make_shared<MaskingSubscriber>(m_drone.get());
-    spinner.add_node(masking_subscriber);
+    masking_spinner->add_node(masking_subscriber);
 
-    suave_log << "Starting spinner" << std::endl;
+    suave_log << "Starting SLAM" << std::endl;
 
-    spinner.start_in_thread();
+    realsense_task->start_in_thread();
+    sleep(3);
+    rtabmap_task->start_in_thread();
 
-    sleep(10)
+    suave_log << "Move the drone around to create initial map" << std::endl;
+    await_confirmation;
 
-    suave_log << "Stopping spinner" << std::endl;
+    suave_log << "Starting VIO" << std::endl;
+    vio_spinner->start_in_thread();
 
-    spinner.stop();
+    sleep(30);
+
+    suave_log << "Stopping task" << std::endl;
+
+    for (auto& task: m_task) {
+        task->stop();
+    }
 
     suave_log << "Exiting..." << std::endl;
 
 }
 
 void SuaveMaskingController::shutdown() {
+    for (auto& task: m_task) {
+        task->stop();
+    }
 }
--- src/suave_launch/src/controllers/SuaveMaskingController.h
+++ src/suave_launch/src/controllers/SuaveMaskingController.h
@@ -8,6 +8,7 @@
 #include "../common/ISuaveController.h"
 #include "../mavutil/Drone.h"
 #include "../mavutil/MavUtil.h"
+#include "../common/ITask.h"
 
 class SuaveMaskingController: public ISuaveController {
 public:
@@ -27,6 +28,7 @@ public:
 
 private:
     std::unique_ptr<Drone> m_drone{ nullptr };
+    std::vector<std::shared_ptr<ITask>> m_task{};
 };
 
 #endif //SUAVEMASKINGCONTROLLER_H
